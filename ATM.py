b613b8a449ebc54632f09a82a5d88d323a1a237d
tiberius <tiberius@NCC-1701.local>
2022-08-03 17:37:35 +0200

    Adds a naive implementation of the ATM with test cases

--- ATM.py
+++ ATM.py
@@ -1,16 +1,29 @@
-
-
+# Python OOO CheatSheet: 
 # https://www.w3schools.com/python/python_classes.asp
 
+# Naive "ATM" that only dispenses unlimited 10 and 20 bills
 class ATM:
 
+    # TODO cater for available bills
     _available_bills = None
 
     # Remember: in python all method signature receive a self as 1st param
     def withdraw(self, amount):
-        print("withdraw")
-        return 0
+        
+        if not isinstance(amount, int):
+            return -1
+
+        intAmount = int(amount)
+        if (intAmount <= 0):
+            return -1
+
+        notes20 = intAmount / 20
+        notes10 = (intAmount % 20) / 10
+
+        return_value = "£20x"+str(int(notes20))+" £10x"+str(int(notes10))
+
+        return return_value
+
 
     def __init__(self, available_bills):
-        print("constructor")
         self._available_bills = available_bills
--- TestATM.py
+++ TestATM.py
@@ -1,24 +1,41 @@
 import unittest
-
 from ATM import ATM
 
 # https://docs.python.org/3/library/unittest.html
 
 class TestATM(unittest.TestCase):
     
+    atm = None
+
+    # Fixture that runs before every test
     @classmethod
     def setUpClass(self):
-        available_bills = {}
-        available_bills = [5, 0]
-        available_bills = [10, 0]
-        available_bills = [20, 0]
-        available_bills = [50, 0]
-        self.atm = ATM(available_bills)
+        self.atm = ATM(None)
+        return 0
 
-    def test_five(self):
-        atm.withdraw(5)
-        self.assertEqual(self.atm.withdraw(5), 5)
+    def test_input_non_integer(self):
+        self.assertEqual(self.atm.withdraw("blah"), -1)
 
+    def test_input_negative(self):
+        self.assertEqual(self.atm.withdraw(0), -1)
+    
+    def test_input_zero(self):    
+        self.assertEqual(self.atm.withdraw(-1), -1)
+
+    def test_10(self):
+        self.assertEqual(self.atm.withdraw(10), "£20x0 £10x1")
+
+    def test_20(self):
+        self.assertEqual(self.atm.withdraw(20), "£20x1 £10x0")
+
+    def test_30(self):
+        self.assertEqual(self.atm.withdraw(30), "£20x1 £10x1")
+
+    def test_40(self):
+        self.assertEqual(self.atm.withdraw(40), "£20x2 £10x0")
+
+    def test_50(self):
+        self.assertEqual(self.atm.withdraw(50), "£20x2 £10x1")
 
 if __name__ == '__main__':
     unittest.main()
